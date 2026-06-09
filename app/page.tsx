45b40105ca8a5bd6537a71428dff706ef1b4040d
Sipu-982 <sipurana38@gmail.com>
2026-06-09 21:55:45 +0530

    new changes made

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,9 @@ import { SkillsSection } from "@/components/sections/SkillsSection";
 import { ExperienceSection } from "@/components/sections/ExperienceSection";
 import { ProjectsSection } from "@/components/sections/ProjectsSection";
 import { ServicesSection } from "@/components/sections/ServicesSection";
-import { AchievementsSection } from "@/components/sections/AchievementsSection";
-import { TestimonialsSection } from "@/components/sections/TestimonialsSection";
-import { BlogSection } from "@/components/sections/BlogSection";
+// import { AchievementsSection } from "@/components/sections/AchievementsSection";
+// import { TestimonialsSection } from "@/components/sections/TestimonialsSection";
+// import { BlogSection } from "@/components/sections/BlogSection";
 import { ContactSection } from "@/components/sections/ContactSection";
 import { Footer } from "@/components/sections/Footer";
 
